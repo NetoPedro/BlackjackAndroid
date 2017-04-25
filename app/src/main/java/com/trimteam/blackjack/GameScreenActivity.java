3834eb84abdb0f9780a41f1839215b4c6dd46259
NetoPedro <pedroneto_09@hotmail.com>
2017-04-25 01:55:01 +0100

    UI elements added

--- app/src/main/java/com/trimteam/blackjack/GameScreenActivity.java
+++ app/src/main/java/com/trimteam/blackjack/GameScreenActivity.java
@@ -2,18 +2,25 @@ package com.trimteam.blackjack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.GridView;
 
 public class GameScreenActivity extends AppCompatActivity {
 
     private Board mBoard;
-
+    GridLayout IAHandGrid, userHandGrid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
         startComponents();
         showCards();
+        IAHandGrid = (GridLayout) findViewById(R.id.IAHand);
+        userHandGrid = (GridLayout) findViewById(R.id.userHand);
+
     }
+
+    
 
     /**
      * Method to display user and IA cards.
