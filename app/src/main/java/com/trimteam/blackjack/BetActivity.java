902a7743a388932b373eb66f5f47bd9617c80852
NetoPedro <pedroneto_09@hotmail.com>
2017-05-12 16:10:32 +0100

    Bet activity implementation started

---
+++ app/src/main/java/com/trimteam/blackjack/BetActivity.java
@@ -0,0 +1,13 @@
+package com.trimteam.blackjack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bet);
+    }
+}
